621231adb757ffd6dabbcf76eddb8750af96f353
Joe Bender <joebenderweb@gmail.com>
2020-01-06 10:41:46 -0500

    Add logger.py

---
+++ logger.py
@@ -0,0 +1,10 @@
+class Logger:
+    def __init__(dir, filename):
+        self.dir = dir
+        self.filename = filename
+    
+    def log(self, loss):
+        pass
+
+    def close(self):
+        pass
