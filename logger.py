0c823c8d78a7430bea7fed08d113b781b973e653
Joe Bender <joebenderweb@gmail.com>
2020-01-09 08:40:41 -0500

    Add more parameters and formatting to log()

--- logger.py
+++ logger.py
@@ -2,8 +2,8 @@ class Logger:
     def __init__(self, filename):
         self.f = open(filename, 'w')
     
-    def log(self, loss):
-        self.f.write(str(loss) + '\n')
+    def log(self, epoch, batch, train_loss, val_loss, train_metric, val_metric):
+        self.f.write("{},{},{},{},{},{}\n".format(epoch, batch, train_loss, val_loss, train_metric, val_metric))
 
     def close(self):
         self.f.close()
